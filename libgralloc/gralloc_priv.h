0a56cd47b8fe560a838ffd5fefd74d4bdc27220d
Tommy Alex <iptux7@gmail.com>
2014-06-09 10:52:02 +0800

    gralloc_priv.h: add some define to support TARGET_USES_QCOM_BSP
    Change-Id: Ib5b3d08e14cbd93b198f3652063f7c1c3a8a98bf
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -80,6 +80,12 @@ enum {
      */
     GRALLOC_USAGE_PRIVATE_CP_BUFFER       =       0x00400000,
 
+    /* This flag is used for SECURE display usecase */
+    GRALLOC_USAGE_PRIVATE_SECURE_DISPLAY  =       0x00800000,
+
+    /* Buffer content should be displayed on an primary display only */
+    GRALLOC_USAGE_PRIVATE_INTERNAL_ONLY   =       0x00010000,
+
     /* Legacy heaps - these heaps are no-ops so we are making them zero
      * The flags need to be around to compile certain HALs which have
      * not cleaned up the code
